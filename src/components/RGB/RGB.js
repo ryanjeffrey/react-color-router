2a6b157dbefbed06e399bcc21bf21c714acca928
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-27 17:18:45 -0500

    Add the swoosh ✅

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -6,6 +6,7 @@ export default function RGB() {
   return (
     <>
       <div className="background" style={{ background: `rgb(${r}, ${g}, ${b})` }}>
+        <img src={process.env.PUBLIC_URL + '/nike.png'} />
         <p>{`rgb(${r}, ${g}, ${b})`}</p>
       </div>
     </>
