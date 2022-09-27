b04b71dff5d35f7924754aa82ac5acfbe146f413
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-27 13:44:28 -0500

    Create three nav links for bg color presets

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
 import './NavBar.css';
 
 export default function NavBar() {
   return (
-    <nav>NavBar</nav>
+    <header>
+      <nav>
+        <NavLink className='navy' to='/rgb/10/22/42'>
+            Navy
+        </NavLink>
+        <NavLink className='orange' to='/rgb/199/56/4'>
+            Orange
+        </NavLink>
+        <NavLink className='grey' to='/rgb/238/238/238'>
+            Grey
+        </NavLink>
+      </nav>
+    </header>
   );
 }
